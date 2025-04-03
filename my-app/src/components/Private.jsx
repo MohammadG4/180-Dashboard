7d719a9e866aadc6f49991d62cdf1a563a3082d9
Mohammad Gafour <mohammadgafourg4@gmail.com>
2025-04-04 00:22:06 +0200

    Delete my-app directory

--- my-app/src/components/Private.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
-
-const Private = () => {
-  const token=localStorage.getItem("token");
-  return token?<Outlet/>:<Navigate to="/login" />
-}
-
-export default Private
